15c5a9bda5e838e9a9ad0035ede8bbed2433cf41
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-08-03 15:55:52 +0200

    Updating routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
 
   def home
+    @projects = Project.all
+    render 'projects/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show, :edit, :update] do
-    resources :projects
-  end
+  resources :users, only: [:show, :edit, :update]
+
+  resources :projects, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 
   root to: "pages#home"
 end
